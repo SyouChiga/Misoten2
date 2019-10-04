b484a96eb151585fb966b0eb1dbd7636b1ea4104
YuichiAkagawa <akagawa9686@gmail.com>
2019-10-04 15:16:27 +0900

    プロジェクト作成

---
+++ Source/Misoten2/Misoten2.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Misoten2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Misoten2, "Misoten2" );
